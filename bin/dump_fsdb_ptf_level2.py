334239c5826485b7510af186ff2cd1c783a71d63
Serge Monkewitz <smm@ipac.caltech.edu>
2015-06-24 13:41:36 -0700

    Do not 0-pad PTF field and CCD identifiers to produce directory names.

--- bin/dump_fsdb_ptf_level2.py
+++ bin/dump_fsdb_ptf_level2.py
@@ -65,8 +65,8 @@ def write_paths(conn_string, table, writer):
                     # Construct path relative to link tree from filename
                     f = os.path.join(m.group('ptffield')[:4],
                                      m.group('ptffield'),
-                                     m.group('fid'),
-                                     m.group('ccdid'),
+                                     str(int(m.group('fid'))),
+                                     str(int(m.group('ccdid'))),
                                      f)
                     if always_private[i]:
                         # Hide away private files
